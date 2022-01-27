f32fdb1ccddc066f868b5c36d221c861963404e1
Vetagiri-Hrushikesh <58148267+Vetagiri-Hrushikesh@users.noreply.github.com>
2022-01-27 14:51:27 +0530

    Server which sends a resp for client req in chrome.
    Minimal server implemented and code modularity

--- Network Programming/Client/webClient.c
+++ Network Programming/Client/webClient.c
@@ -1,23 +1,4 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <stdarg.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <sys/ioctl.h>
-#include <netdb.h>
-
-#define SERVER_PORT 80
-#define MAXLINE 4096
-#define SA struct sockaddr
-
-void err_n_die(const char *fmt, ...);
+#include "common.h"
 
 int main(int argc, char **argv)
 {
@@ -51,38 +32,11 @@ int main(int argc, char **argv)
 
     memset(receiveLine, 0, MAXLINE);
     while ((n = read(sockfd, receiveLine, MAXLINE - 1)) > 0)
-    {
         printf("%s", receiveLine);
-    }
+
     if (n < 0)
         err_n_die("read error");
 
     exit(0);
 }
 
-void err_n_die(const char *fmt, ...)
-{
-    int errno_save;
-    va_list ap;
-
-    // Any system or library call
-    errno_save = errno;
-
-    // Print out the format+err to standard out
-    va_start(ap, fmt);
-    vfprintf(stdout, fmt, ap);
-    fprintf(stdout, "\n");
-    fflush(stdout);
-
-    // Print out the error message set by errno.
-    if (errno_save != 0)
-    {
-        fprintf(stdout, "(errno = %d) : %s\n", errno_save, strerror(errno_save));
-        fprintf(stdout, "\n");
-        fflush(stdout);
-    }
-    va_end(ap);
-
-    // This is the and_die part terminates with an error.
-    exit(1);
-}
